509a3f619bb6b83e760179bf0ea4ae3a0c95210d
Fahad Oni <fahadoni272@gmail.com>
2026-07-27 01:38:52 +0600

    completed task 13-5b

--- problem-solve.js
+++ problem-solve.js
@@ -125,3 +125,38 @@ function removeFirstAndLast(str) {
 }
 
 console.log(removeFirstAndLast('hello'));
+
+
+// Task 13-5A: Check for a Palindrome
+// Write a function isPalindrome that takes a string and returns true if it reads the same forwards and backwards, otherwise false. Assume lowercase, no spaces.
+
+function isPalindrome(str) {
+  let reversedString = str.split('').reverse().join('');
+
+  if (str === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome('level'));
+console.log(isPalindrome('hello'));
+
+
+// Task 13-5B: Find the Shortest Word in a Sentence
+// Write a function findShortestWord that takes a sentence and returns the shortest word in it.
+
+
+function findShortestWord(str) {
+  let word = str.split(' ')
+  let shortest = word[0];
+  for (let i = 0; i <= word.length - 1; i++){
+    if (shortest.length > word[i].length) {
+      shortest = word[i];
+    }    
+  }
+  return shortest;
+}
+
+console.log(findShortestWord('JavaScript is a fun language'));
